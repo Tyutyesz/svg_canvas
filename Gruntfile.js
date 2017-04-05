c393b576725e1d50f3a196977879191f0dadd3be
Tyutyesz <cs.matyi@hotmail.com>
2017-04-05 20:14:08 +0200

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+/**
+ * Created by csmat on 2017. 02. 20..
+ */
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: {
+            dist: {
+                files: {
+                    'public/assets/styles/style.css' : 'public/assets/styles/style.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: '**/*.scss',
+                tasks: ['sass']
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default',['watch']);
+}
